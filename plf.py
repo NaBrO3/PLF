1b072f183b022c79ddb92889fb9526a12a941bb4
vang1ong7ang <vang1ong7ang@outlook.com>
2020-06-09 13:49:05 +0000

    dev

--- plf.py
+++ plf.py
@@ -3,8 +3,7 @@ from typing import Iterable
 from numbers import Number
 from collections import namedtuple
 from itertools import combinations
-from numpy import matrix
-from numpy import any as npany
+from sympy.matrices import Matrix
 
 
 class Vector(tuple):
@@ -17,49 +16,63 @@ class Point(namedtuple('Point', ['x', 'y'])):
         return super().__new__(self, x, y)
 
 
-class Edge(namedtuple('Edge', ['s', 't'])):
-    def __new__(self, s: Point, t: Point) -> Edge:
-        assert len(s.x) == len(t.x)
-        assert len(s.y) == len(t.y)
-        assert s.x != t.x
-        return super().__new__(self, s, t)
+class Piece(tuple):
+    def __new__(self, *args: Iterable[Point]) -> Piece:
+        assert len(set([len(arg.y) for arg in args])) == 1
+        assert len(set([arg.x for arg in args])) == len(args)
+        assert all([len(arg.x)+1 == len(args) for arg in args])
+        return super().__new__(self, args)
 
 
 class PLF(tuple):
-    def __new__(self, *args: Iterable[Edge]) -> PLF:
+    def __new__(self, *args: Iterable[Piece]) -> PLF:
         # TODO: validation
         return super().__new__(self, args)
 
-    def __call__(self, *args: Iterable[Number], error: Number = 0) -> Iterable[Number]:
+    def __call__(self, *args: Iterable[Number]) -> Iterable[Number]:
         # TODO: validation
 
-        dim = len(args)
-        np = dim + 1
-        ne = dim*np//2
-        for pack in combinations(self, ne):
-            points = set()
-            for edge in pack:
-                points.add(edge.s)
-                points.add(edge.t)
-            if len(points) != np:
+        for piece in self:
+            r = Matrix([arg for arg in args] + [1])
+            X = [point.x for point in piece]
+            S = [point for point in zip(*X)]
+            R = Matrix(S + [[1]*len(piece)])
+            l = R.inv() * r
+            if max(l) > 1:
                 continue
-
-            points = [point for point in points]
-
-            r = [arg for arg in args] + [1]
-            r = matrix(r).T
-            R = [[point.x[i] for point in points]
-                 for i in range(len(args))] + [[1] * np]
-            Ry = [point.y for point in points]
-            Ry = matrix(Ry)
-            Ry = Ry.T
-            R = matrix(R)
-            l = R.I * r
-            if npany(l > 1+1e-10):
+            if min(l) < 0:
                 continue
-            if npany(l < -1e-10):
-                continue
-            ry = Ry * l
-            return Vector(*ry.T.tolist()[0])
+            Y = [point.y for point in piece]
+            T = Matrix([point for point in zip(*Y)])
+            y = T * l
+            return Vector(*y)
 
         raise AssertionError
+
+
+plf = PLF(
+    Piece(
+        Point(
+            Vector(1, 1),
+            Vector(8),
+        ),
+        Point(
+            Vector(2, 3),
+            Vector(5),
+        ),
+        Point(
+            Vector(3, 2),
+            Vector(5),
+        ),
+    ),
+
+
+)
+
+print(plf)
+ret = plf(1, 1)
+print(ret)
+ret = plf(2, 1.5)
+print(ret)
+ret = plf(2.5, 2)
+print(ret)
